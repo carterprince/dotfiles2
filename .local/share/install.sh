0e7ccd20faaf1d26952dd6f2a3542e63e0668cb4
Carter Prince <carteraprince@gmail.com>
2024-01-05 02:07:50 -0500

    bash -> sh

--- .local/share/install.sh
+++ .local/share/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run this file from the Arch ISO with:
-# curl -LO carterprince.us/install.sh && bash install.sh
+# curl -LO carterprince.us/install.sh && sh install.sh
 
 clear # for dramatic effect
 
