7192f39b296c64a89a440ed8a480d62728431b05
Carter Prince <carteraprince@gmail.com>
2024-06-15 11:53:19 -0400

    fix various stuff

--- .local/share/install.sh
+++ .local/share/install.sh
@@ -20,12 +20,11 @@ get_input() {
 HOSTNAME=$(get_input "Enter hostname" "desktop")
 
 # define some packages
-MISC="neovim curl git chromium mpv mpv-mpris nsxiv xsel nerd-fonts-sf-mono adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-sans-cn-fonts man-db man-pages wikiman dashbinsh imagemagick htop neofetch expac bat gvfs-mtp android-tools fd baobab better-adb-sync-git gimp playerctl reflector cronie jdk-openjdk"
+MISC="neovim curl git chromium mpv mpv-mpris ttf-monaco-nerd-font-git nsxiv xsel nerd-fonts-sf-mono adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-sans-cn-fonts man-db man-pages wikiman dashbinsh imagemagick htop neofetch expac bat gvfs-mtp android-tools fd baobab better-adb-sync-git gimp playerctl reflector cronie jdk-openjdk papirus-icon-theme"
 NETWORKING="dhcpcd networkmanager"
 # LATEX="texlive-latex texlive-latexextra texlive-fontsrecommended"
 LATEX=""
-# GNOME="gnome-shell nautilus gnome-tweaks gnome-control-center gdm xdg-user-dirs papirus-icon-theme gnome-shell-extension-dash-to-dock xdg-desktop-portal-gnome gnome-shell-extension-stealmyfocus-git" # more minimal GNOME install
-GNOME="gnome gnome-tweaks"
+GNOME="gnome gnome-tweaks dconf-editor adw-gtk3"
 CHIPSET=$(lscpu | grep -iq "amd" && echo "amd" || echo "intel")
 PACKAGES="$NETWORKING $CHIPSET-ucode $MISC $LATEX $GNOME" # this is what will be installed
 
