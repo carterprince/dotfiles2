0db5b9da907d3f4e92f97013c9f9081e787027b6
Carter Prince <carteraprince@gmail.com>
2024-07-07 13:51:59 -0400

    update

--- organize.bash
+++ organize.bash
@@ -4,5 +4,5 @@ mkdir -p ~/{Pictures,Documents,Videos,Audio}
 
 mv {~/Downloads,~}/*.{png,bmp,tiff,jpg,jpeg,gif,webp,svg} ~/Pictures
 mv {~/Downloads,~}/*.{pdf,docx,doc,odt,txt,org,norg,org\#,org\~} ~/Documents
-mv {~/Downloads,~}/*.{mp4,webm} ~/Videos
+mv {~/Downloads,~}/*.{mp4,webm,mkv} ~/Videos
 mv {~/Downloads,~}/*.{mp3,flac,wav} ~/Audio
